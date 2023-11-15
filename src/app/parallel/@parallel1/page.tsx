10319b41d0ed88ffed274d4a6c7006270158f218
khg <poohghg@mrblue.com>
2023-11-15 17:27:19 +0900

    test: 컴포넌트 테스트

---
+++ src/app/parallel/@parallel1/page.tsx
@@ -0,0 +1,9 @@
+const Parallel1 = () => {
+    return (
+        <>
+            <h1>Parallel1</h1>
+        </>
+    );
+};
+
+export default Parallel1;
---
+++ src/app/parallel/@parallel2/page.tsx
@@ -0,0 +1,9 @@
+const Parallel2 = () => {
+    return (
+        <>
+            <h1>Parallel2</h1>
+        </>
+    );
+};
+
+export default Parallel2;
